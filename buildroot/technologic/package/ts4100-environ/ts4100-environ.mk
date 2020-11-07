ee1185908e0214bee4275f45174a66d088eb3e92
Kris Bahnsen <kris@embeddedarm.com>
2020-11-06 20:20:52 -0800

    buildroot/package/ts4100_environ: Removed site method
    Left over from testing, causes issues with buildroot

--- buildroot/technologic/package/ts4100-environ/ts4100-environ.mk
+++ buildroot/technologic/package/ts4100-environ/ts4100-environ.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 TS4100_ENVIRON_AUTORECONF = YES
-TS4100_ENVIRON_SITE_METHOD = git
 TS4100_ENVIRON_VERSION = v1.0
 TS4100_ENVIRON_SITE = $(call github,embeddedarm,ts4100-environ-dc,$(TS4100_ENVIRON_VERSION))
 
