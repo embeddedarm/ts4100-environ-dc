029a2d5850fb3d1c6921ca78338417b88396f91a
Kris Bahnsen <kris@embeddedarm.com>
2020-12-03 16:55:28 -0800

    buildroot/board: Add U-Boot script to image
    Need to remove "rw" from the default command line so the SD card
    gets mounted RO by default. Use a U-Boot script in /boot/ to
    modify the env just before booting starts

--- buildroot/technologic/board/ts4100-environ/post-build.sh
+++ buildroot/technologic/board/ts4100-environ/post-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+MKIMAGE=$HOST_DIR/bin/mkimage
+
 # Install network interfaces files
 install -m 0644 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/interfaces "${TARGET_DIR}"/etc/network/
 install -m 0644 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/wpa_supplicant.conf "${TARGET_DIR}"/etc/
@@ -7,3 +9,7 @@ install -m 0644 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/wp
 # Install startup scripts
 install -m 0755 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/S35loadwifilcdmodules "${TARGET_DIR}"/etc/init.d/
 install -m 0755 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/S37startenviron "${TARGET_DIR}"/etc/init.d/
+
+# Create U-Boot script in image, create both .ub and .scr for compat sake
+$MKIMAGE -A arm -T script -C none -n 'readonly' -d "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/tsinit.source "${TARGET_DIR}"/boot/boot.scr
+$MKIMAGE -A arm -T script -C none -n 'readonly' -d "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/tsinit.source "${TARGET_DIR}"/boot/boot.ub
